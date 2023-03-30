09c9b53b707323ee472747dc8b3b5baf41016092
AsmitaShivajiTagad <asmitatagad07@gmail.com>
2023-03-30 16:10:52 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import style from './App.module.css';
+
+let ranNo = Math.floor(Math.random() * 100) + 1;
 
 function App() {
+  const [Guess, setGuess] = useState('');
+  const [count, setCount] = useState(0);
+  const [randomNo, setRandomNo] = useState(ranNo);
+  const [msg, setMsg] = useState('');
+
+  function handleChange(e) {
+    setGuess(e.target.value);
+  }
+
+  function handleClick() {
+    if (Number(randomNo) === Number(Guess)) {
+      setMsg("Congratulations!! You Guess Lucky Number");
+    }
+
+    if (randomNo < Guess) {
+      alert("You Gussed a grater number");
+    }
+
+    if (randomNo > Guess) {
+      alert("You Gussed a smaller number");
+    }
+
+    setCount(count + 1);
+    setGuess('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.container}>
+
+      <p>{ranNo}</p>
+      <p>{msg}</p>
+      <h1>Guess lucky Number Game</h1>
+      <input type='Number' placeholder='Guss Lucky Number...' value={Guess} onChange={handleChange} />
+      <button onClick={handleClick}>Match Number</button>
+
     </div>
   );
 }
